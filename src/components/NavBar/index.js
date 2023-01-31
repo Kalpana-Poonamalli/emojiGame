2a92ed8f154ac07ef606faa13ecd1ab60ef1d691
Kalpana-Poonamalli <kalpanapoonamalli963@gmail.com>
2023-01-31 06:46:36 +0000

    emojiGame

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -6,24 +6,24 @@ const NavBar = props => {
 
   return (
     <div>
-      {score !== 12 ? (
-        <div className="navbar-container">
-          <div className="navbar-items">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
-              alt="emoji logo"
-              className="navbar-logo"
-            />
-            <h1 className="navbar-heading">Emoji Game</h1>
-          </div>
+      <div className="navbar-container">
+        <div className="navbar-items">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+            alt="emoji logo"
+            className="navbar-logo"
+          />
+          <h1 className="navbar-heading">Emoji Game</h1>
+        </div>
+        {score === 12 ? (
+          ''
+        ) : (
           <div className="navbar-items">
             <p className="score">Score: {score}</p>
             <p className="top-score">Top Score: {topScore}</p>
           </div>
-        </div>
-      ) : (
-        ''
-      )}
+        )}
+      </div>
     </div>
   )
 }
--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -4,65 +4,41 @@ import './index.css'
 const WinOrLoseCard = props => {
   const {score, playTheGameAgain} = props
 
+  const wonOrNot = score === 12 ? 'You Won' : 'You Lose'
+  const bScore = score === 12 ? 'Best Score' : 'Score'
+  const image =
+    score === 12
+      ? 'https://assets.ccbp.in/frontend/react-js/won-game-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/lose-game-img.png'
+
   const playAgain = () => {
     playTheGameAgain(score)
   }
 
   return (
     <div>
-      {score === 12 ? (
-        <div className="win-container won-2">
-          <div className="rows">
-            <div className="center">
-              <h1 className="lose">You Won</h1>
-              <p className="score">Best Score</p>
-              <p id="score" className="final-score">
-                {score}/12
-              </p>
-              <button
-                type="button"
-                className="play-again"
-                onClick={playAgain}
-                data-testid="playAgain"
-              >
-                Play Again
-              </button>
-            </div>
-            <div className="img">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/won-game-img.png"
-                alt="win or lose"
-                className="lose-img"
-              />
-            </div>
+      <div className="win-container">
+        <div className="rows">
+          <div className="center">
+            <h1 className="lose">{wonOrNot}</h1>
+            <p className="score">{bScore}</p>
+            <p id="score" className="final-score">
+              {score}/12
+            </p>
+            <button
+              type="button"
+              className="play-again"
+              onClick={playAgain}
+              data-testid="playAgain"
+            >
+              Play Again
+            </button>
+          </div>
+          <div className="img">
+            <img src={image} alt="win or lose" className="lose-img" />
           </div>
         </div>
-      ) : (
-        <div className="win-container">
-          <div className="rows">
-            <div className="center">
-              <h1 className="lose">You Lose</h1>
-              <p className="score">Score</p>
-              <p className="final-score">{score}/12</p>
-              <button
-                type="button"
-                className="play-again"
-                onClick={playAgain}
-                data-testid="playAgain"
-              >
-                Play Again
-              </button>
-            </div>
-            <div className="img">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/lose-game-img.png"
-                alt="win or lose"
-                className="lose-img"
-              />
-            </div>
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   )
 }
